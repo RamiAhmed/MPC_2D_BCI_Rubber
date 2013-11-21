cda9a1a2b843df284dda27b30d73aa1d5db3ab72
Rami Ahmed Bock <RamiAhmedBock@gmail.com>
2013-11-21 14:29:55 +0100

    Changed GameController to FixedUpdate
    - In order to try to ensure consistent speed of blinking (stimulus
    frequency)

--- Assets/scripts/GameController.cs
+++ Assets/scripts/GameController.cs
@@ -45,7 +45,7 @@ public class GameController : MonoBehaviour {
 	}
 	
 	// Update is called once per frame
-	void Update() {
+	void FixedUpdate() {
 		gameTime += Time.deltaTime;
 
 		if (currentCop == null) {
