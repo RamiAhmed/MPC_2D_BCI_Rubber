1d27f8a64310f56688782892132d4c20b1009a1c
Rami Ahmed Bock <RamiAhmedBock@gmail.com>
2013-11-21 10:06:01 +0100

    Cleared all meta data
    - Also added more code to OSCListener
    - Listener now tries to parse OSC message as float and write to debug
    log (console)

--- Assets/scripts/UnityOSCListener.cs
+++ Assets/scripts/UnityOSCListener.cs
@@ -5,13 +5,33 @@ using System.Collections;
 using System.Collections.Generic;
 
 public class UnityOSCListener : MonoBehaviour  {
-	public void OSCMessageReceived(OSC.NET.OSCMessage message){	
+	
+	public float SignalThreshold = 0.5f;
+	
+	public void OSCMessageReceived(OSC.NET.OSCMessage message){ 
 		string address = message.Address;
 		ArrayList args = message.Values;
-
-		Debug.Log(address);
-		foreach( var item in args){
-			Debug.Log(item);
+		
+		/*
+        Debug.Log(address);
+        foreach( var item in args){
+            Debug.Log(item);
+        }
+        */
+		
+		if (address.Equals("sig")) {
+			foreach (object signal in args) {
+				float value;
+				if (float.TryParse(signal.ToString(), out value)) {
+					Debug.Log("Signal value: " + value);
+				}
+				else {
+					Debug.LogWarning("Signal was not float");
+				}
+			}
+		}
+		else {
+			Debug.LogWarning("Received unknown OSC address: " + address);
 		}
 	}
 }
